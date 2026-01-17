bf19b714966f59bc5cda88511bd06eee28c55f4e
Valentino Gagliardi <valentino@valentinog.com>
2026-01-17 16:32:59 +0100

    Set up project

---
+++ cypress.config.mjs
@@ -0,0 +1,23 @@
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+	e2e: {
+		baseUrl: "http://localhost:3000",
+		specPattern: "**/*.feature",
+		async setupNodeEvents(on, config) {
+			await addCucumberPreprocessorPlugin(on, config);
+
+			on(
+				"file:preprocessor",
+				createBundler({
+					plugins: [createEsbuildPlugin(config)],
+				}),
+			);
+
+			return config;
+		},
+	},
+});
